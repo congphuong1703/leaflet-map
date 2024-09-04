93912bc796d51e026b9c2d97506f893fd60d4838
Công Phương <congphuong.vietnam62@gmail.com>
2024-09-04 13:19:55 +0700

    feat: add tiles

--- src/layers/CustomLayer.js
+++ src/layers/CustomLayer.js
@@ -7,8 +7,8 @@ const CustomLayer = () => {
             url={`${process.env.REACT_APP_URL_LAYER}/{z}/{x}/{y}.png`}
             attribution='NPC contributors'
             tms={1}
-            minZoom={12}
-            maxZoom={17}
+            minZoom={15}
+            maxZoom={18}
         />
     )
 }
